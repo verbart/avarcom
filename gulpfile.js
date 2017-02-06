6ad8324323646a45b5491db4a6f0720b33e40bef
Artem Verbat <artem.verbat@gmail.com>
2017-02-06 07:48:38 +0200

    Added Webpack for build scripts.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,5 @@
 const gulp = require('gulp');
-const babelify = require('babelify');
-const browserify = require('browserify');
-const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
-const uglify = require('gulp-uglify');
 const postcss = require('gulp-postcss');
 const stylus = require('gulp-stylus');
 const cleanCSS = require('gulp-clean-css');
@@ -18,6 +14,7 @@ const gutil = require('gulp-util');
 const pug = require('gulp-pug');
 const tinypng = require('gulp-tinypng-nokey');
 const spritesmith = require('gulp.spritesmith');
+const webpack = require('webpack');
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -56,21 +53,19 @@ gulp.task('styles', function () {
         .pipe(gulp.dest('./public/css'))
 });
 
-gulp.task('scripts', function() {
-    return browserify({
-        entries: './src/app.js',
-        debug: isDevelopment
-    })
-        .transform(babelify, {presets: ['es2015']})
-        .bundle()
-        .on('error', function(error) {
-            gutil.log(gutil.colors.red('Error: ' + error), '\n', error.codeFrame);
-            this.emit('end');
-        })
-        .pipe(source('bundle.js'))
-        .pipe(buffer())
-        .pipe(gulpIf(!isDevelopment, uglify({mangle: false})))
-        .pipe(gulp.dest('./public/js'));
+gulp.task('scripts', function(done) {
+    return webpack(require('./webpack.config.js'), function(err, stats) {
+        if(err) throw new gutil.PluginError('webpack', err);
+
+        gutil.log('[scripts]', stats.toString({
+            colors: gutil.colors.supportsColor,
+            chunks: false,
+            hash: false,
+            version: false
+        }));
+
+        done();
+    });
 });
 
 gulp.task('sprite', function() {
