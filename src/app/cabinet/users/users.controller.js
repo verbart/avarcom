930c1f82158c94f8cd04f9b0528d49d0f08c457d
Artem Verbat <artem.verbat@gmail.com>
2017-04-30 22:03:49 +0300

    Added controller for user creation. Refactoring for redesing.

--- src/app/cabinet/users/users.controller.js
+++ src/app/cabinet/users/users.controller.js
@@ -5,39 +5,10 @@ export default class {
     this.User = User;
 
     this.getUsers(this.selectedDate);
-    this.rolesOptions = {
-      // selectionLimit: 1,
-      styleActive: true,
-      displayProp: 'label',
-      showCheckAll: false,
-      showUncheckAll: false
-    };
-    this.roles = [{id: 1, label: "David"}, {id: 2, label: "Jhon"}, {id: 3, label: "Danny"} ];
-    // this.selectedRole = [this.roles[0]];
-    this.selectedRole = [];
 
-    this.onRoleSelect = (role) => {
-      console.log(role);
-
-      this.selectedRoleLabel = this.selectedRole.map(item => item.label).join(', ');
-    };
     // const interval = $interval(() => this.getUsers(), 60*1000);
     // $scope.$on('$destroy', () => $interval.cancel(interval));
-    $http.get(CONSTANT.API_URL+'/levels').then(response => {
-      console.log(response);
-      $http.get(CONSTANT.API_URL+'/levels/1/roles').then(response => {
-        console.log(response);
-        $http.get(CONSTANT.API_URL+'/roles/1/cities').then(response => {
-          console.log(response);
-        }, error => {
-          console.log(error);
-        });
-      }, error => {
-        console.log(error);
-      });
-    }, error => {
-      console.log(error);
-    });
+
   }
 
   getUsers() {
--- src/components/modals/new-user/new-user.component.js
+++ src/components/modals/new-user/new-user.component.js
@@ -5,16 +5,110 @@ export default {
     close: '&',
     dismiss: '&'
   },
-  controller: function () {
+  controller: function ($http, CONSTANT) {
     this.$onInit = () => {
-      this.cities = this.resolve.cities;
-      this.selectedCity = this.cities[0];
+      this.typesOptions = {
+        selectionLimit: 1,
+        styleActive: true,
+        displayProp: 'name',
+        showCheckAll: false,
+        showUncheckAll: false,
+        closeOnSelect: true
+      };
+      this.rolesOptions = {
+        selectionLimit: 1,
+        styleActive: true,
+        displayProp: 'name',
+        showCheckAll: false,
+        showUncheckAll: false,
+        closeOnSelect: true
+      };
+      this.citiesOptions = {
+        styleActive: true,
+        displayProp: 'name',
+        showCheckAll: false,
+        showUncheckAll: false
+      };
+
+      this.loadTypes();
+    };
+
+    this.loadTypes = () => {
+      $http.get(CONSTANT.API_URL+'/levels').then(response => {
+        console.log(response);
+        this.types = response.data;
+        this.selectedType = [this.types[0]];
+        this.buildTypeLabel();
+        this.loadRoles(this.types[0]);
+      }, error => {
+        console.log(error);
+      });
+    };
+    this.loadRoles = (type) => {
+      if (type) {
+        $http.get(`${CONSTANT.API_URL}/levels/${type.id}/roles`).then(response => {
+          console.log(response);
+          this.roles = response.data;
+          this.selectedRole = [this.roles[0]];
+          this.buildRoleLabel();
+          this.loadCities(this.roles[0]);
+        }, error => {
+          console.log(error);
+        });
+      } else {
+        this.roles = [];
+        this.selectedRole = [];
+        this.buildRoleLabel();
+        this.loadCities();
+      }
+    };
+    this.loadCities = (role) => {
+      if (role) {
+        $http.get(`${CONSTANT.API_URL}/roles/${role.id}/cities`).then(response => {
+          this.citiesOptions.selectionLimit = 1;
+          this.cities = response.data;
+          this.selectedCities = [this.cities[0]];
+          this.buildCitiesLabel();
+        }, error => {
+          console.log(error);
+        });
+      } else {
+        this.cities = [];
+        this.selectedCities = [];
+        this.buildCitiesLabel();
+      }
+    };
+
+    this.buildTypeLabel = () => {
+      this.selectedTypeLabel = this.selectedType.map(item => item.name).join(', ');
+    };
+    this.buildRoleLabel = () => {
+      this.selectedRoleLabel = this.selectedRole.map(item => item.name).join(', ');
+    };
+    this.buildCitiesLabel = () => {
+      this.selectedCitiesLabel = this.selectedCities.map(item => item.name).join(', ');
+    };
+
+    this.onTypeChanged = () => {
+      this.buildTypeLabel();
+      this.loadRoles(this.selectedType[0]);
+    };
+    this.onRoleChanged = () => {
+      this.buildRoleLabel();
+      this.loadCities(this.selectedRole[0]);
+    };
+    this.onCitiesChanged = () => {
+      this.buildCitiesLabel();
+    };
+    this.onCitiesSelected = (city) => {
+      if (this.selectedRole[0] && !this.selectedRole[0].multi_cities) {
+        this.selectedCities = [city];
+      }
     };
 
     this.ok = () => {
-      this.close({$value: this.result});
+      this.close({$value: this.user});
     };
-
     this.cancel = () => {
       this.dismiss({$value: 'cancel'});
     };
