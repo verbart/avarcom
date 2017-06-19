36e9299c586d046ffd945f927006f65e1a578a1f
Artem Verbat <artem.verbat@gmail.com>
2017-06-19 14:22:21 +0300

    Fixed auth: logout if user data is empty

--- src/components/main-header/main-header.component.js
+++ src/components/main-header/main-header.component.js
@@ -1,7 +1,11 @@
 export default {
   templateUrl: 'views/components/main-header/main-header.html',
   controller: class {
-    constructor($rootScope) {
+    constructor($rootScope, $state, AuthData) {
+      this.userData = AuthData.get();
+
+      if (!this.userData) $state.go('logout');
+
       $rootScope.$on('$stateChangeSuccess', () => this.navbarIsActive = false);
     }
   }
