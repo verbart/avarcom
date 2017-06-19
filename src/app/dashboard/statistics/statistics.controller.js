838bd0ecc75897f1ad053d3db9eaa1ca29f2d947
Artem Verbat <artem.verbat@gmail.com>
2017-06-19 17:13:48 +0300

    Changed spaces size from 4 to 2

--- src/app/dashboard/statistics/statistics.controller.js
+++ src/app/dashboard/statistics/statistics.controller.js
@@ -1,27 +1,27 @@
 export default class StatisticsCtrl {
-    constructor(moment, Statistics, $scope) {
-        this.$scope = $scope;
-        this.Statistics = Statistics;
-        this.startDate = moment().subtract(1, 'day');
-        this.endDate = moment();
+  constructor(moment, Statistics, $scope) {
+    this.$scope = $scope;
+    this.Statistics = Statistics;
+    this.startDate = moment().subtract(1, 'day');
+    this.endDate = moment();
 
-        // $scope.$watchGroup([()=>this.startDate, ()=>this.endDate], () => {
-        //     this.getStats();
-        // });
-    }
+    $scope.$watchGroup([()=>this.startDate, ()=>this.endDate], () => {
+      this.getStats();
+    });
+  }
 
-    getStats() {
-        this.Statistics.get({
-            startTime: this.startDate.format('X'),
-            endTime: this.endDate.format('X'),
-            type: 'general'
-        }, response => {
-            console.log(response);
-            this.general = response.general_data;
-            this.commissioners = response.commissars;
-            this.rating = angular.copy(this.commissioners);
-        }, error => {
-            console.log(error);
-        });
-    }
+  getStats() {
+    this.Statistics.get({
+      startTime: this.startDate.format('X'),
+      endTime: this.endDate.format('X'),
+      type: 'general'
+    }, response => {
+      console.log(response);
+      this.general = response.general_data;
+      this.commissioners = response.commissars;
+      this.rating = angular.copy(this.commissioners);
+    }, error => {
+      console.log(error);
+    });
+  }
 }
